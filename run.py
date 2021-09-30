abbbf7e354092c61345c3b2c20aa421afe4b3d2e
NadieFiind <nadiefiind@gmail.com>
2021-09-30 20:15:23 +0800

    Improve Entry Point

--- run.py
+++ run.py
@@ -1,4 +1,18 @@
+import sys
 from src import shell
 
 if __name__ == "__main__":
-	shell.main()
+	try:
+		arg1 = sys.argv[1]
+	except IndexError:
+		print("Not enough arguments supplied.\n")
+		print("Usage:")
+		print("  python run.py [src]")
+		print("\nsrc:")
+		print("  shell - Run an interactive shell.")
+		exit()
+	
+	if arg1 == "shell":
+		shell.main()
+	else:
+		print(f"src '{arg1}' not found.")
